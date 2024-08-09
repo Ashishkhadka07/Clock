6f52bf5bd11e17ebdf99c52ddbf8148e717af1bb
Ashish Khadka <aasheeshkhadka@gmail.com>
2024-08-09 19:54:34 +0545

    analog clock added

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function updateClock() {
     const now = new Date()
     let hours = now.getHours()
@@ -14,3 +15,42 @@ function updateClock() {
 
 updateClock()
 setInterval(updateClock, 1000)
+
+var numberHours = document.querySelector(".number_hours");
+var secondsBar = document.querySelector(".seconds_bar");
+
+var numberElement = [];
+var barElement = [];
+
+for (let i = 1; i <= 12; i++) {
+    numberElement.push(`<span style="--index:${i}"><p>${i}</p></span>`);
+}
+numberHours.insertAdjacentHTML("afterbegin", numberElement.join(""));
+
+for (let i = 1; i <= 60; i++) {
+    barElement.push(`<span style="--index:${i}"><p></p></span>`);
+}
+secondsBar.insertAdjacentHTML("afterbegin", barElement.join(""));
+
+// Time
+const handHours = document.querySelector(".hand.hour");
+const handMinutes = document.querySelector(".hand.minute");
+const handSeconds = document.querySelector(".hand.second");
+
+function getCurrentTime() {
+    let date = new Date();
+    let currentHours = date.getHours();
+    let currntMinutes = date.getMinutes();
+    let currentSeconds = date.getSeconds();
+
+    handHours.style.transform = `rotate(${currentHours * 30 + currntMinutes / 2}deg)`;
+    handMinutes.style.transform = `rotate(${currntMinutes * 6}deg)`;
+    handSeconds.style.transform = `rotate(${currentSeconds * 6}deg)`;
+
+    // Sound effect for second hand
+    let sound = new Audio("./audio/sound.wav");
+    sound.play();
+}
+
+getCurrentTime();
+setInterval(getCurrentTime, 1000); // 1000ms = 1s
